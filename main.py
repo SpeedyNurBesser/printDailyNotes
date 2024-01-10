e51dc33f0b4b28d5bde68d47be647c5b2340f5bf
Ole370 <70910518+SpeedyNurBesser@users.noreply.github.com>
2024-01-10 01:50:59 +0100

    overhauled TwentyNinthFeb Function

--- main.py
+++ main.py
@@ -23,11 +23,11 @@ for i in range(0, 3):
 
 def TwentyNinthFeb(year: int):
     # returns true if the given year is a leap year
-    if year % 100 == 0:
-        if year % 400 == 0: return True
-        else: return False
-    elif year % 4 == 0:
-        return True
+    if year % 4 == 0:
+        if year % 100 == 0:
+            if year % 400 == 0: return True
+            else: return False
+        else: return True
     else: return False
 
 def initDate(date: str):
